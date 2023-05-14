05967a9de56b17e831dda7454c4cfd3fca070af5
Lizbeth Castro <lizbethcastro010@gmail.com>
2023-05-14 13:47:40 -0600

    starter code

---
+++ assets/js/index.js
@@ -0,0 +1,29 @@
+var startButton = document.getElementById("begin")
+
+var questions = [
+    {
+        queston: 'What is Tony Stark without a suit?',
+        option1: '1) Playboy, Smart, Philanthropist, Millionaire',
+        option2: '2) Genius, Billonaire, Playboy, Philanthropist',
+        option3: '3) Nothing',
+        answer: '2'
+    },
+    {
+        question: 'What house does Harry Potter belong to?',
+        option1: '1) Slytherin',
+        option2: '2) Ravenclaw',
+        option3: '3) Gryffindor',
+        answer: '3'
+    },
+    {
+        question: 'Are you also taking a coding class?',
+        option1: '1) Yes',
+        option2: '2) No',
+        option3: '3) Huh?',
+        answer: '1'
+    },
+];
+
+startButton.addEventListener("click", function () {
+    
+})
