6e35c67b8d33b2d99460835d11aedf0a4dc68ace
Lizbeth Castro <lizbethcastro010@gmail.com>
2023-05-27 14:40:31 -0600

    Updated questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ var startButton = document.getElementById("begin");
 var timer = document.getElementById("timer");
 var quizSection = document.getElementById("quizSection");
 var question = document.getElementById("question");
-var answerA = document.getElementById("answerA");
-var answerB = document.getElementById("answerB");
-var answerC = document.getElementById("answerC");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
 var initialsForm = document.getElementById("initialsForm");
 var scoreSection = document.getElementById("scoreSection");
 var intials = document.getElementById("intials");
@@ -15,25 +15,32 @@ var end = document.getElementById("end");
 // Questions
 var questions = [
     {
-        q: "What does HTML stand for?",
-        a: "A) Hypertext Markup Language",
-        b: "B) Higher Text Markup Language",
-        c: "C) Hypertext Multi Language",
-        answer: 'a'
+        q: 'What does HTML stand for?',
+        one: "1) Hypertext Markup Language",
+        two: "2) Higher Text Markup Language",
+        three: "3) Hypertext Multi Language",
+        answer: '1'
     },
     {
         q: 'What does CSS stand for?',
-        a: 'A) Computer Styling Sheets',
-        b: 'B) Cascading Style Sheets',
-        c: 'C) Colorful Super Sheets',
-        answer: 'b'
+        one: '1) Computer Styling Sheets',
+        two: '2) Cascading Style Sheets',
+        three: '3) Colorful Super Sheets',
+        answer: '2'
     },
     {
         q: 'Are you also taking a coding class?',
-        a: 'A) Huh?',
-        b: 'B) No',
-        c: 'C) Yes',
-        answer: 'c'
+        one: 'A) Huh?',
+        two: 'B) No',
+        three: 'C) Yes',
+        answer: '3'
     },
 ];
 
+score = 0;
+timerCount = 10;
+questionCount = 0;
+function renderPageLoad () {
+    quizSection.style.display = "none";
+    end.style.display = "none";
+}
